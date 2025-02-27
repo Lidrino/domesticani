1054f8cc3c3d440c5636c0e5479fbb104b56a03e
Lidrino <zasypkin.alexey00@gmail.com>
2025-02-27 16:36:58 +0300

    fix: navbar animation

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -1,12 +1,23 @@
-// Сразу создаём переменные
-let navbar = document.querySelector('.nav_bar').classList
+// Cоздаём переменные
+let navbar = document.querySelector(".nav_bar").classList
+
+let nav_a = document.querySelectorAll(".nav_a")
 let active_class = "navbar_scrolled"
 
-/**
- * Слушаем событие прокрутки
- */
-window.addEventListener('scroll', e => {
-    console.log(scrollY)
-  if(scrollY > 600) navbar.add(active_class)
-  else navbar.remove(active_class)
+
+/* Слушаем событие прокрутки */
+window.addEventListener("scroll",function(){
+    let y = this.scrollY;
+    if(y>600){
+      navbar.add("navbar_scrolled")
+        for(let item of nav_a) {
+            item.classList.add("nav_a_scrolled")
+        }
+    }
+    else{
+      navbar.remove("navbar_scrolled")
+        for(let item of nav_a) {
+            item.classList.remove("nav_a_scrolled")
+        }
+    }
 })
