1615eeda8954105b2c5af8f5e4fcf230ddc83a1a
Aleksey <zasypkin.alexey00@gmail.com>
2025-03-01 19:36:44 +0300

    fix: fix animation

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -1,6 +1,6 @@
 // Cоздаём переменные
 let navbar = document.querySelector(".nav_bar").classList
-
+let wrapper = document.querySelector(".wrapper").classList
 let nav_a = document.querySelectorAll(".nav_a")
 let active_class = "navbar_scrolled"
 
@@ -10,12 +10,14 @@ window.addEventListener("scroll",function(){
     let y = this.scrollY;
     if(y>600){
       navbar.add("navbar_scrolled")
+      wrapper.add("wrapper_scrolled")
         for(let item of nav_a) {
             item.classList.add("nav_a_scrolled")
         }
     }
     else{
       navbar.remove("navbar_scrolled")
+      wrapper.remove("wrapper_scrolled")
         for(let item of nav_a) {
             item.classList.remove("nav_a_scrolled")
         }
